e161eba76c999a33ddcecb7a77c0e4dfef50e0d4
Rishabh Aggarwal <rishabhaggarwal@urbancompany.com>
2022-11-12 12:33:17 +0530

    hero section changes

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
+import Nav from 'react-bootstrap/Nav';
 
 function IndividualIntervalsExample() {
   return (
     <Carousel id="home" nextLabel='' prevLabel='' indicators={true} slide={false}
-    autoPlay={true}
-    interval={3000}
+      autoPlay={true}
+      interval={3000}
     >
       <Carousel.Item>
         <img
@@ -14,7 +15,7 @@ function IndividualIntervalsExample() {
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <Nav.Link href="/#learn"><div className="carousel-caption-custom">Get Started!</div></Nav.Link>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -24,17 +25,7 @@ function IndividualIntervalsExample() {
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="./hero-bg.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <Nav.Link href="/#contact"><div className="carousel-caption-custom">Help us Improve!</div></Nav.Link>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
